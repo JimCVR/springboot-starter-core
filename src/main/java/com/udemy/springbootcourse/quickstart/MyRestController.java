a43157ad25064604bcb4b710058922e317894444
JimCVR <jaimecvr96@gmail.com>
2024-02-01 13:00:34 +0100

    Refactor package name

--- src/main/java/com/udemy/springbootcourse/quickstart/MyRestController.java
+++ src/main/java/com/udemy/springbootcourse/quickstart/MyRestController.java
@@ -1,4 +1,4 @@
-package com.udemy.springbootcourse.rest;
+package com.udemy.springbootcourse.quickstart;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
